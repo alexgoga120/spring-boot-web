b53eb9e2e37a322a6e27ef3c3df7938d6410adbb
aÃlexgoga120 <alexgoga120@gmail.com>
2023-04-27 14:35:06 -0600

    simple list model in table

--- src/main/java/com/alexgoga120/springboot/app/web/controllers/IndexController.java
+++ src/main/java/com/alexgoga120/springboot/app/web/controllers/IndexController.java
@@ -1,9 +1,14 @@
 package com.alexgoga120.springboot.app.web.controllers;
 
+import com.alexgoga120.springboot.app.web.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/app")
@@ -13,5 +18,32 @@ public class IndexController {
     public String index(Model model) {
         model.addAttribute("titulo", "hello spring");
         return "index/index";
+    }
+    @GetMapping("/profile")
+    public String profile(Model model) {
+        User user = new User();
+        user.setName("Alex");
+//        user.setLastname("Gomez");
+        model.addAttribute("titulo", "Profile page");
+        model.addAttribute("user", user);
+        return "profile/index";
+    }
+    @GetMapping("/list")
+    public String list(Model model) {
+        model.addAttribute("titulo", "list page");
+        return "profile/list";
+    }
+
+    @ModelAttribute("users")
+    public List<User> fillUsers(){
+
+        List<User> users = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            User user = new User();
+            user.setName("name " + i );
+            user.setLastname("lastname " + i);
+            users.add(user);
+        }
+        return users;
     }
 }
